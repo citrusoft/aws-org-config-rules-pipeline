dc2132c7481cdd1000a928976ba47528d4636a5c
Thomas Hunt <thunt@citrusoft.com>
2020-01-07 06:48:25 -0800

    cfg-rules: Initial infrastructure for CodePipeline to deploy Organization Config Rules.

---
+++ deploy-xaccount-roles.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Filename    : deploy-xaccount-roles.sh
+# Date        : 10 Jan 2020
+# Author      : Tommy Hunt (tahv@pge.com)
+# Description : Deploys Cross-account roles that allow OrganizationConfigRule, VPCFlowLogS3Enforcement, 
+#               to detect, correct and report non-compliant VPC FlowLogging.
+#
+echo Cross-account roles in ToolAccount
+aws cloudformation deploy --stack-name custom-config-xaccount-roles \
+ --template-file MemberAccount/01-custom-config-xaccount-roles.yaml \
+ --capabilities CAPABILITY_NAMED_IAM \
+ --profile betsy
+echo Cross-account roles in MemberAccount
+aws cloudformation deploy --stack-name custom-config-xaccount-roles \
+ --template-file MemberAccount/01-custom-config-xaccount-roles.yaml \
+ --capabilities CAPABILITY_NAMED_IAM \
+ --profile julio
+echo Cross-account roles in DevAccount
+aws cloudformation deploy --stack-name custom-config-xaccount-roles \
+ --template-file MemberAccount/01-custom-config-xaccount-roles.yaml \
+ --capabilities CAPABILITY_NAMED_IAM \
+ --profile thunt
+# echo Cross-account roles in ComplianceAccount
+# aws cloudformation deploy --stack-name custom-config-xaccount-roles \
+#  --template-file MemberAccount/01-custom-config-xaccount-roles.yaml \
+#  --capabilities CAPABILITY_NAMED_IAM \
+#  --profile tahunt
--- single-click-cross-account-pipeline.sh
+++ single-click-cross-account-pipeline.sh
@@ -1,43 +1,171 @@
 #!/usr/bin/env bash
-echo -n "Enter ToolsAccount > "
-read ToolsAccount
-echo -n "Enter ToolsAccount ProfileName for AWS Cli operations> "
-read ToolsAccountProfile
-echo -n "Enter Dev Account > "
-read DevAccount
-echo -n "Enter DevAccount ProfileName for AWS Cli operations> "
-read DevAccountProfile
-echo -n "Enter Test Account > "
-read TestAccount
-echo -n "Enter TestAccount ProfileName for AWS Cli operations> "
-read TestAccountProfile
-echo -n "Enter Prod Account > "
-read ProdAccount
-echo -n "Enter ProdAccount ProfileName for AWS Cli operations> "
-read ProdAccountProfile
 
-aws cloudformation deploy --stack-name pre-reqs --template-file ToolsAcct/pre-reqs.yaml --parameter-overrides DevAccount=$DevAccount TestAccount=$TestAccount ProductionAccount=$ProdAccount --profile $ToolsAccountProfile
-echo -n "Enter S3 Bucket created from above > "
-read S3Bucket
+if [ "$1" = "-B" ]; then
+  batch=true
+else
+  batch=false
+fi
 
-echo -n "Enter CMK ARN created from above > "
-read CMKArn
+# This is a general-purpose function to ask Yes/No questions in Bash, either
+# with or without a default answer. It keeps repeating the question until it
+# gets a valid answer.
 
-echo -n "Executing in DEV Account"
-aws cloudformation deploy --stack-name toolsacct-codepipeline-role --template-file DevAccount/toolsacct-codepipeline-codecommit.yaml --capabilities CAPABILITY_NAMED_IAM --parameter-overrides ToolsAccount=$ToolsAccount CMKARN=$CMKArn --profile $DevAccountProfile
+ask() {
+    local prompt default reply
 
-echo -n "Executing in TEST Account"
-aws cloudformation deploy --stack-name toolsacct-codepipeline-cloudformation-role --template-file TestAccount/toolsacct-codepipeline-cloudformation-deployer.yaml --capabilities CAPABILITY_NAMED_IAM --parameter-overrides ToolsAccount=$ToolsAccount CMKARN=$CMKArn  S3Bucket=$S3Bucket --profile $TestAccountProfile
+    if [ "${2:-}" = "Y" ]; then
+        prompt="Y/n"
+        default=Y
+    elif [ "${2:-}" = "N" ]; then
+        prompt="y/N"
+        default=N
+    else
+        prompt="y/n"
+        default=
+    fi
 
-echo -n "Executing in PROD Account"
-aws cloudformation deploy --stack-name toolsacct-codepipeline-cloudformation-role --template-file TestAccount/toolsacct-codepipeline-cloudformation-deployer.yaml --capabilities CAPABILITY_NAMED_IAM --parameter-overrides ToolsAccount=$ToolsAccount CMKARN=$CMKArn  S3Bucket=$S3Bucket --profile $ProdAccountProfile
+    while true; do
 
+        # Ask the question (not using "read -p" as it uses stderr not stdout)
+        echo -n "$1 [$prompt] "
 
-echo -n "Creating Pipeline in Tools Account"
-aws cloudformation deploy --stack-name sample-lambda-pipeline --template-file ToolsAcct/code-pipeline.yaml --parameter-overrides DevAccount=$DevAccount TestAccount=$TestAccount ProductionAccount=$ProdAccount CMKARN=$CMKArn S3Bucket=$S3Bucket --capabilities CAPABILITY_NAMED_IAM --profile $ToolsAccountProfile
+        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+        read reply </dev/tty
 
-echo -n "Adding Permissions to the CMK"
-aws cloudformation deploy --stack-name pre-reqs --template-file ToolsAcct/pre-reqs.yaml --parameter-overrides CodeBuildCondition=true --profile $ToolsAccountProfile
+        # Default?
+        if [ -z "$reply" ]; then
+            reply=$default
+        fi
 
-echo -n "Adding Permissions to the Cross Accounts"
-aws cloudformation deploy --stack-name sample-lambda-pipeline --template-file ToolsAcct/code-pipeline.yaml --parameter-overrides CrossAccountCondition=true --capabilities CAPABILITY_NAMED_IAM --profile $ToolsAccountProfile
+        # Check if the reply is valid
+        case "$reply" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+
+    done
+}
+
+if $batch ; then
+	ToolsAccount=782391863272
+	ToolsAccountProfile=betsy
+	DevAccount=123133550781
+	DevAccountProfile=thunt
+	ComplianceAccount=567207295412
+	ComplianceAccountProfile=tahunt
+	MasterAccount=919568423267
+	MasterAccountProfile=tah
+else
+	default=782391863272
+	read -p "Enter ToolsAccount [$default]> " ToolsAccount
+	ToolsAccount=${ToolsAccount:-$default}
+	default=betsy
+	read -p "Enter ToolsAccount ProfileName for AWS Cli operations [$default]> " ToolsAccountProfile
+	ToolsAccountProfile=${ToolsAccountProfile:-$default}
+	default=123133550781
+	read -p "Enter Dev Account [$default]> " DevAccount
+	DevAccount=${DevAccount:-$default}
+	default=thunt
+	read -p "Enter DevAccount ProfileName for AWS Cli operations [$default]> " DevAccountProfile
+	DevAccountProfile=${DevAccountProfile:-$default}
+	default=567207295412
+	read -p "Enter ComplianceAccount [$default]> " ComplianceAccount
+	ComplianceAccount=${ComplianceAccount:-$default}
+	default=tahunt
+	read -p "Enter ComplianceAccount ProfileName for AWS Cli operations [$default]> " ComplianceAccountProfile
+	ComplianceAccountProfile=${ComplianceAccountProfile:-$default}
+	default=919568423267
+	read -p "Enter Master Account [$default]> " MasterAccount
+	MasterAccount=${MasterAccount:-$default}
+	default=tah
+	read -p "Enter MasterAccount ProfileName for AWS Cli operations [$default]> " MasterAccountProfile
+	MasterAccountProfile=${MasterAccountProfile:-$default}
+fi
+echo -e "ToolsAccount=$ToolsAccount \t ToolsAccountProfile=$ToolsAccountProfile"
+echo -e "DevAccount=$DevAccount \t DevAccountProfile=$DevAccountProfile"
+echo -e "ComplianceAccount=$ComplianceAccount \t ComplianceAccountProfile=$ComplianceAccountProfile"
+echo -e "MasterAccount=$MasterAccount \t MasterAccountProfile=$MasterAccountProfile"
+
+if $batch || ask "Create S3 Bucket and KMS CMS Key and alias in ToolsAccount? " N; then
+	aws cloudformation deploy --stack-name aws-org-config-rules-pre-reqs  \
+	--template-file ToolsAcct/pre-reqs.yaml \
+	--parameter-overrides DevAccount=$DevAccount ComplianceAccount=$ComplianceAccount MasterAccount=$MasterAccount \
+	--profile $ToolsAccountProfile
+fi
+
+read -p "Enter S3 Bucket created from above > " S3Bucket
+read -p "Enter CMK ARN created from above > " CMKArn
+
+if $batch || ask "Create CodeCommit roles for CodePipeline in DevAccount? " N; then
+	aws cloudformation deploy --stack-name toolsacct-codepipeline-role \
+	--template-file DevAccount/toolsacct-codepipeline-codecommit.yaml \
+	--capabilities CAPABILITY_NAMED_IAM \
+	--parameter-overrides ToolsAccount=$ToolsAccount CMKARN=$CMKArn \
+	--profile $DevAccountProfile
+fi
+
+if $batch || ask "Create CloudFormation deployment roles in ComplianceAccount? " N; then
+	aws cloudformation deploy --stack-name toolsacct-codepipeline-cloudformation-role \
+	--template-file ComplianceAccount/toolsacct-codepipeline-cloudformation-deployer.yaml \
+	--capabilities CAPABILITY_NAMED_IAM \
+	--parameter-overrides MasterAccount=$MasterAccount ToolsAccount=$ToolsAccount CMKARN=$CMKArn  S3Bucket=$S3Bucket \
+	--profile $ComplianceAccountProfile
+fi
+
+if $batch || ask "Create CloudFormation deployment roles in MasterAccount? " N; then
+	aws cloudformation deploy --stack-name toolsacct-codepipeline-cloudformation-role \
+	--template-file MasterAccount/toolsacct-codepipeline-cloudformation-deployer.yaml \
+	--capabilities CAPABILITY_NAMED_IAM \
+	--parameter-overrides ToolsAccount=$ToolsAccount CMKARN=$CMKArn  S3Bucket=$S3Bucket \
+	--profile $MasterAccountProfile
+fi
+
+if $batch || ask "Create CodePipeline and build roles in ToolsAccount? " N; then
+	aws cloudformation deploy --stack-name aws-org-config-rules-pipeline \
+	--template-file ToolsAcct/code-pipeline.yaml \
+	--parameter-overrides DevAccount=$DevAccount ComplianceAccount=$ComplianceAccount MasterAccount=$MasterAccount CMKARN=$CMKArn S3Bucket=$S3Bucket --capabilities CAPABILITY_NAMED_IAM \
+	--capabilities CAPABILITY_NAMED_IAM \
+	--profile $ToolsAccountProfile
+fi
+
+if $batch || ask "Deploy Cross Account Roles to ToolsAccount? " N; then
+	aws cloudformation deploy --stack-name custom-config-xaccount-roles \
+	 --template-file MemberAccount/01-custom-config-xaccount-roles.yaml \
+	 --capabilities CAPABILITY_NAMED_IAM \
+	 --profile betsy
+fi
+if $batch || ask "Deploy Cross-account roles to MemberAccount? " N; then
+	aws cloudformation deploy --stack-name custom-config-xaccount-roles \
+	 --template-file MemberAccount/01-custom-config-xaccount-roles.yaml \
+	 --capabilities CAPABILITY_NAMED_IAM \
+	 --profile julio
+fi
+if $batch || ask "Deploy Cross-account roles to DevAccount? " N; then
+	aws cloudformation deploy --stack-name custom-config-xaccount-roles \
+	 --template-file MemberAccount/01-custom-config-xaccount-roles.yaml \
+	 --capabilities CAPABILITY_NAMED_IAM \
+	 --profile thunt
+fi
+# if $batch || ask "Deploy Cross-account roles to ComplianceAccount? " N; then
+# 	aws cloudformation deploy --stack-name custom-config-xaccount-roles \
+# 	 --template-file MemberAccount/01-custom-config-xaccount-roles.yaml \
+# 	 --capabilities CAPABILITY_NAMED_IAM \
+# 	 --profile tahunt
+# fi
+
+echo -e "\n######### Reconfiguring resources with X-Account roles. ########"
+
+if $batch || ask "Add Permissions to the CMK? " N; then
+	aws cloudformation deploy --stack-name aws-org-config-rules-pre-reqs  \
+	--template-file ToolsAcct/pre-reqs.yaml \
+	--parameter-overrides CodeBuildCondition=true \
+	--profile $ToolsAccountProfile
+fi
+
+if $batch || ask "Add Permissions to the Cross Accounts ?" N; then
+	aws cloudformation deploy --stack-name aws-org-config-rules-pipeline \
+	--template-file ToolsAcct/code-pipeline.yaml \
+	--parameter-overrides CrossAccountCondition=true \
+	--capabilities CAPABILITY_NAMED_IAM \
+	--profile $ToolsAccountProfile
+fi
